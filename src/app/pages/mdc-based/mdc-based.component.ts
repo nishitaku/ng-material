e8ab9dac7fe591e918f37415db54839881d8e0f2
nishitaku <29690066+nishitaku@users.noreply.github.com>
2024-12-25 23:50:53 +0900

    feat: add button components to MdcBasedComponent

--- src/app/pages/mdc-based/mdc-based.component.ts
+++ src/app/pages/mdc-based/mdc-based.component.ts
@@ -1,14 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-mdc-based',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './mdc-based.component.html',
   styleUrls: ['./mdc-based.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdcBasedComponent {
-
-}
+export class MdcBasedComponent {}
