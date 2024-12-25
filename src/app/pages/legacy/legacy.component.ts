cda2fac3f4b4e4e3922cfe1385145b64a29065c5
nishitaku <29690066+nishitaku@users.noreply.github.com>
2024-12-26 00:20:54 +0900

    feat: add checkbox

--- src/app/pages/legacy/legacy.component.ts
+++ src/app/pages/legacy/legacy.component.ts
@@ -1,14 +1,62 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyCheckboxModule } from '@angular/material/legacy-checkbox';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-legacy',
   standalone: true,
-  imports: [MatLegacyButtonModule, MatDividerModule, MatIconModule],
+  imports: [
+    MatLegacyButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatLegacyCheckboxModule,
+    FormsModule,
+    NgFor,
+  ],
   templateUrl: './legacy.component.html',
   styleUrls: ['./legacy.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LegacyComponent {}
+export class LegacyComponent {
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      { name: 'Primary', completed: false, color: 'primary' },
+      { name: 'Accent', completed: false, color: 'accent' },
+      { name: 'Warn', completed: false, color: 'warn' },
+    ],
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete =
+      this.task.subtasks != null &&
+      this.task.subtasks.every((t) => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return (
+      this.task.subtasks.filter((t) => t.completed).length > 0 &&
+      !this.allComplete
+    );
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach((t) => (t.completed = completed));
+  }
+}
--- src/app/pages/mdc-based/mdc-based.component.ts
+++ src/app/pages/mdc-based/mdc-based.component.ts
@@ -1,14 +1,62 @@
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-mdc-based',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatCheckboxModule,
+    FormsModule,
+    NgFor,
+  ],
   templateUrl: './mdc-based.component.html',
   styleUrls: ['./mdc-based.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdcBasedComponent {}
+export class MdcBasedComponent {
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      { name: 'Primary', completed: false, color: 'primary' },
+      { name: 'Accent', completed: false, color: 'accent' },
+      { name: 'Warn', completed: false, color: 'warn' },
+    ],
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete =
+      this.task.subtasks != null &&
+      this.task.subtasks.every((t) => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return (
+      this.task.subtasks.filter((t) => t.completed).length > 0 &&
+      !this.allComplete
+    );
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach((t) => (t.completed = completed));
+  }
+}
